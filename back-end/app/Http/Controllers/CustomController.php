11ec66a6dd50de8a6051d1ea7e4974b29f32e143
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2026-03-01 22:22:04 +0100

    Controleur fais pour custom

--- back-end/app/Http/Controllers/CustomController.php
+++ back-end/app/Http/Controllers/CustomController.php
@@ -9,4 +9,19 @@ class CustomController extends Controller
         // Affiche la page custom formulaire
         return view('custom');
     }
+
+    public function saveOptions()
+    {
+        // Sauvegarde en SESSION
+        session([
+            'background_color' => request('background_color'),
+            'border_color' => request('border_color'),
+            'border_style' => request('border_style'),
+            'word_break' => request('word_break'),
+            'word_spacing' => request('word_spacing'),
+        ]);
+    
+        // Redirige vers la page custom
+        return redirect()->route('custom');
+    }
 }
